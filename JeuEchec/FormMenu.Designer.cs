3eb1cc864575a5da9c4d118170e8b2e3a90d3ecd
Felix Rondeau <felix.rondeau@hotmail.com>
2025-03-19 10:52:19 -0400

    Ajout d'un bouton

--- JeuEchec/FormMenu.Designer.cs
+++ JeuEchec/FormMenu.Designer.cs
@@ -23,22 +23,34 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new System.Windows.Forms.Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(411, 46);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(105, 45);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(570, 610);
-            Name = "Form1";
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(570, 610);
+            Controls.Add(button1);
             Text = "Form1";
             ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button1;
 
         #endregion
     }
